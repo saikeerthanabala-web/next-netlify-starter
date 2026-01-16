45b77cb055d026e1ca8a60741e9a374acced2f86
Sai Keerthana Bala <saikeerthanabala@gmail.com>
2026-01-16 11:26:27 -0800

    Updated title and header text in index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>SaiKeerthana Bala</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="SAI KEERTHANA BALA" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Automation Test Engineer</code>
         </p>
       </main>
 
